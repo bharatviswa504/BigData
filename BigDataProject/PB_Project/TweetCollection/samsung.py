487aa096a84e3301925f71fb172a10fdc625cd6f
bharatviswa504 <viswabharat504@gmail.com>
2014-12-18 12:32:22 -0600

    Big Data Project Visualization using D3 and Analyzing using mongodb

---
+++ BigDataProject/PB_Project/TweetCollection/samsung.py
@@ -0,0 +1,32 @@
+import pymongo
+from pymongo import MongoClient
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import time
+ckey = 'HiV1yNqUzK8mQQ4syTVcnFVRK'
+csecret = 'HlEZ4G7hYfJqQvAzQUj9tLllX4nfn1Hdc3bE8KzjyC8XqL0xcA'
+atoken = '2771116699-HpksvxtwTkBmZKbAvYOqHU7aXMXGRGZGCEP9ifA'
+asecret = 'AGi7K4OL7ymdV3H3TbnLVfUPiwuebECWZhunOOaVWKpvi'
+
+class listener(StreamListener):
+	
+	def on_data(self,data):
+		try:        
+			collection.insert({'tweet':data})
+			return True
+		except BaseException, e:
+			print 'failed writing,', str(e)
+			time.sleep(5)
+			
+	
+	def on_error(self, status):
+		print status
+client = MongoClient('localhost', 27017)
+db = client.samsung
+collection = db.samsung_tweet_data
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track = ["samsung" | "galaxys5" | "galaxys6"])
+
